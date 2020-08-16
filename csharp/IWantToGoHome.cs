c9f067c64431c32535eb72a9172ea290266b1e9c
integraldx <integraldx@integraldx.me>
2020-08-16 12:20:55 +0900

    add: CSharp

---
+++ csharp/IWantToGoHome.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace IWantToGoHome
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("집에 가고 싶다");
+        }
+    }
+}
